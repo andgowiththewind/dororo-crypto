64ff8b7ce61793cdc45ee6304d37650861fb2cd1
dororo <2414566866@qq.com>
2024-01-07 04:50:37 +0800

    add zTree;

---
+++ web-user-interface-design/src/views/dororocrypto/crypto/AsideMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    AsideMenu.vue
+  </div>
+</template>
+
+<script>
+import {Notification, MessageBox, Message, Loading} from 'element-ui';
+import $ from '@/utils/jquery-vendor';
+
+export default {
+  // 请注意这里的.vue后缀需要去掉
+  name: "AsideMenu",
+  components: {},
+  data() {
+    return {}
+  },
+  methods: {},
+  watch: {},
+  mounted() {
+  },
+}
+</script>
+
+<style scoped>
+</style>
