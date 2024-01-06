65082df35fceffa69e6f50b0d0e139f670350ddb
dororo <2414566866@qq.com>
2024-01-06 20:46:30 +0800

    SpringBoot init;

---
+++ src/main/java/com/dororo/future/dororocrypto/DororoCryptoApplication.java
@@ -0,0 +1,13 @@
+package com.dororo.future.dororocrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DororoCryptoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DororoCryptoApplication.class, args);
+    }
+
+}
